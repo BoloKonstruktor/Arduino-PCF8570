54ce2f804684a24647ad0d2722b7824c357c6203
Łukasz Biernacki <59331462+BoloKonstruktor@users.noreply.github.com>
2020-02-12 11:58:21 +0100

    Update PCF8570.cpp

--- PCF8570.cpp
+++ PCF8570.cpp
@@ -72,42 +72,20 @@ Wire.beginTransmission( this->hwaddr );
 Wire.endTransmission();
 }
 
-void PCF8570::read( uint8_t offset, void* data, uint8_t size ){
+void PCF8570::read( uint8_t& offset, void* data, uint8_t size ){
 uint8_t* buff = (uint8_t*)data;
 
-Wire.beginTransmission( this->hwaddr );
-#if ARDUINO >= 100
-  Wire.write(offset);
-#else
-  Wire.send(offset);
-#endif
-Wire.endTransmission();
-Wire.requestFrom( this->hwaddr, (int)size );
-
-  while ( !Wire.available() );
-
-  for ( uint8_t i = 0; i < size; i++ ){
-#if ARDUINO >= 100
-  buff[i] = Wire.read();
-#else
-  buff[i] = Wire.receive();
-#endif
-  }
-
-Wire.endTransmission();
+    for( uint8_t i = 0; i < size; i++ ) {
+    buff[i] = readByte( offset );
+    offset++;
+    }
 }
 
-void PCF8570::write( uint8_t offset, void* data, uint8_t size ){
+void PCF8570::write( uint8_t& offset, void* data, uint8_t size ){
 const uint8_t* buff = (uint8_t*)data;
-Wire.beginTransmission( this->hwaddr );
-
-#if ARDUINO >= 100
-  Wire.write( offset );
-  Wire.write( buff, size );
-#else
-  Wire.send( offset );
-  Wire.send( buff, size );
-#endif
-
-Wire.endTransmission();
+  
+  for ( uint8_t i = 0; i < size; i++) {
+  writeByte( offset, buff[i] );
+  offset++;
+  }
 }
