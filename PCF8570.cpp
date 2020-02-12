51825a7bb21d5ff3cff8e2d6a5d6fe624ca9cd56
BoloKonstruktor <bolokonstruktor@gmail.com>
2020-02-12 07:02:04 +0100

    Initial commit

---
+++ PCF8570.cpp
@@ -0,0 +1,74 @@
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <Wire.h>
+#include "PCF8570.h"
+
+
+PCF8570::PCF8570( uint8_t addr ){
+this->hwaddr = addr;	
+}
+
+void PCF8570::begin( void ){
+Wire.begin();	
+}
+
+uint8_t PCF8570::readByte( uint8_t offset ){
+uint8_t buff[1];
+readPacket( offset, buff, 1 );
+return (uint8_t)buff[0];
+}
+
+uint8_t PCF8570::writeByte( uint8_t offset, uint8_t data ){
+uint8_t buff[1];
+buff[0] = data;
+writePacket( offset, buff, 1 );
+}
+
+void PCF8570::clearMemory(void){
+  
+  for( uint8_t offset = 0; offset < 256; offset++ ){
+  writeByte( offset, 0 );
+  }
+}
+
+void PCF8570::readPacket( uint8_t offset, uint8_t * buff, uint8_t size ){
+Wire.beginTransmission( this->hwaddr );
+#if ARDUINO >= 100
+  Wire.write((uint8_t)lowByte(offset));
+#else
+  Wire.send((uint8_t)lowByte(offset));
+#endif
+Wire.endTransmission();
+Wire.requestFrom( this->hwaddr, (int)size );
+
+  while ( !Wire.available() );
+
+  for ( uint8_t i = 0; i < size; i++ ){
+#if ARDUINO >= 100
+  buff[i] = Wire.read();
+#else
+  buff[i] = Wire.receive();
+#endif
+  }
+
+Wire.endTransmission();
+}
+
+void PCF8570::writePacket( uint8_t offset, uint8_t * buff, uint8_t size ){
+Wire.beginTransmission( this->hwaddr );
+
+#if ARDUINO >= 100
+  Wire.write( (uint8_t)lowByte(offset) );
+  Wire.write( buff, size );
+#else
+  Wire.send( lowByte(offset) );
+  Wire.send( highByte(offset) );
+  Wire.send( buff, size );
+#endif
+
+Wire.endTransmission();
+}
---
+++ PCF8570.h
@@ -0,0 +1,28 @@
+#ifndef PCF8570_h
+#define PCF8570_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+class PCF8570 {
+	
+	uint8_t hwaddr;
+	
+public:
+
+	PCF8570( uint8_t=0x50 );
+	void begin( void );
+	uint8_t readByte( uint8_t );
+	uint8_t writeByte( uint8_t, uint8_t );
+	void clearMemory( void );
+	void readPacket( uint8_t, uint8_t*, uint8_t );
+	void writePacket( uint8_t, uint8_t*, uint8_t );	
+};
+
+
+
+#endif
