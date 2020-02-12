54cb1dc8be6700f7fb5bd9c63389cbba28520ddf
Łukasz Biernacki <59331462+BoloKonstruktor@users.noreply.github.com>
2020-02-12 14:27:42 +0100

    Update PCF8570.h

--- PCF8570.h
+++ PCF8570.h
@@ -17,12 +17,12 @@ public:
 	PCF8570( uint8_t=0x50 );
 	void begin( void );
 	uint8_t readByte( uint8_t );
-	uint8_t writeByte( uint8_t, uint8_t );
+	void writeByte( uint8_t, uint8_t );
+	uint8_t readMemory( uint8_t, void*, uint8_t );
+	uint8_t writeMemory( uint8_t, void*, uint8_t );
 	void clearMemory( void );
 	void readPacket( uint8_t, uint8_t*, uint8_t );
 	void writePacket( uint8_t, uint8_t*, uint8_t );
-	uint8_t readMemory( uint8_t, void*, uint8_t );
-	uint8_t writeMemory( uint8_t, void*, uint8_t );
 };
 
 
